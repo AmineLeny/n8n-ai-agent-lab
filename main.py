c9f09c51952b2c874489f49ac08da53e2dcc93d7
AmineLeny <amine.laanaya0@gmail.com>
2025-12-24 10:00:34 +0100

    create MCP server

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import chromadb
-client = chromadb.Client()
-collection = client.create_collection(
-name="customer_memory"
-)
+from fastapi import FastAPI
+from mcp.server.fastmcp import FastMCP
+app = FastAPI()
+mcp = FastMCP(name="my_server")
+@mcp.tool()
+def get_order_status(order_id: str):
+    return {
+        "status": "Shipped",
+        "delivery": "2␣days"
+    }
